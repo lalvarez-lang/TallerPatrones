dbf19dd6366d364619ef4aa42932b40eb22c1824
Luis Alvarez <l.alvarez@globant.com>
2025-08-09 14:36:49 -0500

    Ejercicio4

---
+++ Ejercicio4/Entities/Carpeta.cs
@@ -0,0 +1,57 @@
+using FileSystemCompositeExample.Interfaces;
+
+namespace FileSystemCompositeExample.Entities
+{
+
+    public class Carpeta(string name) : IFileSystemComponent
+    {
+        public string Name { get; private set; } = name;
+        private List<IFileSystemComponent> _children = new List<IFileSystemComponent>();
+
+        public long Size
+        {
+            get
+            {
+                long totalSize = 0;
+                foreach (var child in _children)
+                {
+                    totalSize += child.Size;
+                }
+                return totalSize;
+            }
+        }
+
+        public void Add(IFileSystemComponent component)
+        {
+            _children.Add(component);
+        }
+
+        public void Remove(IFileSystemComponent component)
+        {
+            _children.Remove(component);
+        }
+
+        public void Print(int indent)
+        {
+            Console.WriteLine($"{new string(' ', indent)}+ Folder: {Name} (Total Size: {Size} bytes)");
+            foreach (var child in _children)
+            {
+                child.Print(indent + 2);
+            }
+        }
+
+        public void Rename(string newName)
+        {
+            Name = newName;
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine($"Folder {Name} deleted.");
+            foreach (var child in _children)
+            {
+                child.Delete();
+            }
+        }
+    }
+}
